49a33f8ee3b59340abd87277c5bbe674c3040a05
Jacob Haap <git@j3.cx>
2025-01-04 00:32:18 +0100

    add mnemonic.js to replace seeded-mnemonic
    Adding a new mnemonic.js module to replace the functionality of seeded-mnemonic, adapting a few of the functions from the library for internal use

---
+++ src/mnemonic.js
@@ -0,0 +1,39 @@
+const keccak = require('keccak');
+const crypto = require('crypto');
+const { bip39, wordlists } = require('@iacobus/bip39');
+
+const wordlist = wordlists.english;
+
+function generateMnemonic(input, phraseLength, chain) {
+    let hash;
+    if (chain === 'ETH') {
+        hash = keccak('keccak256').update(input).digest('hex');
+    } else if (chain === 'BTC') {
+        hash = crypto.createHash('sha256').update(input).digest('hex');
+    } else if (chain === 'SOL') {
+        hash = crypto.createHash('sha512').update(input).digest('hex');
+    } else {
+        throw new Error(`Invalid chain, available options: ETH, BTC, SOL.`);
+    }
+
+    const ent = bip39.ent.fromHex(hash);
+    let entropyLength;
+    if (phraseLength === 12 || phraseLength === 18 || phraseLength === 24) {
+        entropyLength = phraseLength === 12 ? 128 : phraseLength === 18 ? 192 : 256;
+    } if (phraseLength !== 12 && phraseLength !== 18 && phraseLength !== 24) {
+        console.log("Length:", phraseLength);
+        throw new Error(`Invalid Mnemonic Phrase Length! Must be 12, 18, or 24.`)
+    }
+
+    const entropy = ent.slice(0, entropyLength);
+    const mnemonic = bip39.core.toMnemonic(wordlist, entropy);
+
+    const valid = bip39.core.validate(wordlist, mnemonic);
+    if (!valid) {
+        throw new Error(`Invalid Mnemonic Phrase Generated.`);
+    } if (valid) {
+        return mnemonic
+    }
+}
+
+module.exports = generateMnemonic;
